b119064175443681ea14e42fa51f6c01dd7cf844
mlhnshuvo <mlhnshuvo@gmail.com>
2021-11-19 19:03:47 +0600

    add nav and footer

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 import AdminComponent from "../components/AdminComponents";
+import Footer from "../components/Footer";
 
 const AdminPage = () => {
   return (
     <div>
+      <Navigation />
       <AdminComponent />
+      <Footer />
     </div>
   );
 };
--- client/src/pages/WhoBuyCard.js
+++ client/src/pages/WhoBuyCard.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 import WhoBuyCardCompo from "../components/WhoBuyCard";
+import Footer from "../components/Footer";
 
 const WhoBuyCard = () => {
   return (
     <div>
+      <Navigation />
       <WhoBuyCardCompo />
+      <Footer />
     </div>
   );
 };
